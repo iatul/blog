8d53c9ab61cea42111a1b6927e2f2824f0686405
Atul Kumar <atul.kumar0401@payu.in>
2017-03-12 15:34:40 +0530

    Almost complete

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,11 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 """Main entry point
 """
+
+import logging
 from pyramid.config import Configurator
 
+from blog.common import saved_ini
+from blog.utility.db_initializer import db_initialize
+
+logger = logging.getLogger(__name__)
 
 def main(global_config, **settings):
-    config = Configurator(settings=settings)
-    config.include("cornice")
-    config.scan("blog.views")
-    return config.make_wsgi_app()
 
+    db_initialize(settings)
+    logger.info('DB initialization complete')
+
+    saved_ini.ini_settings = settings  # saving ini  settings so that it can be used anywhere
+
+    logger.info('ini settings saved')
+
+    config = Configurator(settings=settings)
+    config.include('cornice')
+    config.scan('blog.views')
+    return config.make_wsgi_app()
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 """ Cornice services.
 """
+
+import json
+import logging
+import colander
 from cornice import Service
+from pyramid.response import Response
+from blog.app import app
+from blog.utility.exceptions import BlogException
+from blog.utility.functions import valid_payload
+
+blogs = Service(name='blogs', path='/blogs', description='Blog Api')
+blogs_comments = Service(name='comments', path='/blogs/{bid}/comments',
+                         description='Blog Comment Api')
 
 
-hello = Service(name='hello', path='/', description="Simplest app")
 
 
-@hello.get()
-def get_info(request):
+
+@blogs.post(validators=valid_payload)
+def create_blog(request):
+    """Create Blog."""
+
+    params = request.matchdict
+    print params
+    return {'create': 'blog'}
+
+
+@blogs.get()
+def read_blogs(request):
     """Returns Hello in JSON."""
-    return {'Hello': 'World'}
 
+    params = request.matchdict
+    if 'id' in params:
+        # read_blog
+        return {'read': 'blog'}
+    else:
+        # read_blogs use offset
+        return {'read': 'blogs'}
+
+
+@blogs_comments.get()
+def read_comments(request):
+    params = request
+
+    # check bid
+    return {'read': 'comments'}
+
+
+@blogs_comments.post(validators=valid_payload)
+def create_comment(request):
+    params = request
+    return {'create': 'comments'}
+
+
+			
